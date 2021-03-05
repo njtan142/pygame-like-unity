3a8327dfbee0998839940118132f6667bbb742fd
njtan142 <njtan142@gmail.com>
2021-03-05 17:47:31 +0545

    first commit

---
+++ game_object.py
@@ -0,0 +1,64 @@
+import pygame
+
+pygame.init()
+
+
+class GameObject:
+
+    def __init__(self, name, x=0, y=0):
+        self.name = name
+        self.children = {}
+        self.transform = Transform(x, y)
+        self.renderer = Renderer()
+
+    def add_component(self, component, params):
+        if component == "image_renderer":
+            self.renderer.image(params)
+        if component == "text_renderer":
+            self.renderer.text(params)
+
+    def render(self, screen, offsetx=0, offsety=0):
+        screen.blit(self.renderer.to_render.surface,
+                    (self.transform.position.x + offsetx - self.renderer.to_render.surface.get_width() / 2,
+                     self.transform.position.y + offsety - self.renderer.to_render.surface.get_height() / 2)
+                    )
+    def add_child(self, child):
+        self.children[child.name] = child
+
+
+class Transform:
+
+    def __init__(self, x, y):
+        self.position = Position(x, y)
+        print(self.position.x)
+
+
+class Position:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Renderer:
+
+    def __init__(self):
+        pass
+
+    def image(self, params):
+        self.to_render = ImageRenderer(params)
+
+    def text(self, params):
+        self.to_render = TextRenderer(params)
+
+
+class ImageRenderer:
+
+    def __init__(self, params):
+        self.surface = pygame.image.load(params[0]).convert_alpha()
+
+
+class TextRenderer:
+
+    def __init__(self, params):
+        self.surface = params[0].render(params[1], params[2], params[3])
---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from game_object import GameObject
+
+pygame.init()
+container = GameObject("container")
+test = GameObject("test")
+container.add_child(test)
+screen = pygame.display.set_mode((200, 200), pygame.FULLSCREEN|pygame.SCALED)
+font = pygame.font.SysFont(None, 24)
+test.add_component("text_renderer",
+                   (font, "I love you", False, (255, 255, 255))
+                   )
+
+running = True
+while running:
+    # test.transform.position.x += 0.0001
+    print(container.children)
+
+    test.render(screen, screen.get_width()/2, screen.get_height()/2)
+    for event in pygame.event.get():
+        if event.type is pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+    pygame.display.flip()
